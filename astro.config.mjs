eabc474afaf4aa6be51bcc313c2456cb6632709f
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2024-06-16 21:40:01 -0500

    mensaje

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
