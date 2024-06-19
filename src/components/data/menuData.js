81f5a707db94444175d601a09b30dd76bd3eb966
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2024-06-18 21:12:47 -0500

    menu update

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -4,10 +4,10 @@ export const mainMenu = [
      {link:"#proyectos", content:"Proyectos", icon:"carbon:portfolio"},
      {link:"#blog", content:"Blog", icon:"mdi:blog"},
      {link:"#equipo", content:"Equipo", icon:"mdi:account-group"},
-     {link:"#clientes", content:"Clientes", icon:"mdi:blog"},
+     {link:"#clientes", content:"Clientes", icon:"mdi:account-group"},
 ]
 export const menuBlog = [
      {link:"/", content:"Home", icon:"lucide:home"},
      {link:"/#about", content:"About", icon:"octicon:person-24"},
-     {link:"/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"/#proyectos", content:"Proyectos", icon:"carbon:portfolio"},
 ]
